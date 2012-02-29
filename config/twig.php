16545ee68d081c733b2510293824b174bdcd8c1b
Dwayne Charrington <dwaynecharrington@gmail.com>
2012-02-29 13:41:18 +1000

    Added in config file. Fixed up cached vars issue to use new function, added in cache dir as well.

---
+++ config/twig.php
@@ -0,0 +1,13 @@
+<?php
+
+// Twig template extension (default)
+$config['twig.extension'] = ".twig";
+
+// Default template location
+$config['twig.location'] = APPPATH . "views/";
+
+// Twig template caching location
+$config['twig.cache_location'] = APPPATH . "cache/twig/";
+
+// Debug mode turned on or off for Twig?
+$config['twig.debug'] = false;
--- libraries/Twig.php
+++ libraries/Twig.php
@@ -12,9 +12,28 @@ class Twig {
 	{
 		ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . APPPATH . 'third_party/Twig');
         require_once (string) "Autoloader" . EXT;
+
+        // Load the Twig config file
+        $this->CI->config->load('twig');
+
+        // Required Twig paths and variables, modify in config/twig.php
+        $this->_template_dir = config_item('twig.location');
+        $this->_cache_dir    = config_item('twig.cache_location');
+        $this->_debug        = config_item('twig.debug');
         
         Twig_Autoloader::register();
 	}
+
+    /**
+     * Override the default template location
+     *
+     * @param mixed $location
+     * @returns void
+     */
+    public function set_location($location)
+    {
+        $this->_template_dir = $location;
+    }
 	
 	/**
 	* Load the template and return the data
@@ -27,15 +46,19 @@ class Twig {
 	{
 	    $loader = new Twig_Loader_Filesystem($this->_template_dir);
 	    $twig = new Twig_Environment($loader, array('cache' => $this->_cache_dir,'debug' => $this->_debug));
+
+        if (stripos($template, '.') === false) {
+            $template . config_item('twig.extension');
+        }
 	     
 	    $template = $twig->loadTemplate($template);
 	    
-	    if (is_array($data))
+	    if ( is_array($data) )
 	    {
-	        $data = array_merge($data, $this->ci->load->_ci_cached_vars);
+	        $data = array_merge($data, $this->ci->load->get_vars());
 	    }
 	    
-	    if ($return === true)
+	    if ( $return === true )
 	    {
 	        return $template->render($data);
 	    }
@@ -44,5 +67,41 @@ class Twig {
 	        return $template->display($data);
 	    }
 	}
+
+    /**
+     * Parse String
+     * Parse a string and return it as a string or display it
+     *
+     * @param mixed $string
+     * @param mixed $data
+     * @param mixed $return
+     * @returns void
+     */
+    public function parse_string($string, $data = array(), $return = false)
+    {
+        $loader = new Twig_Loader_String($this->_template_dir);
+
+        $twig = new Twig_Environment($loader, array(
+            'cache' => $this->_cache_dir,
+            'debug' => $this->_debug,
+        ));
+
+        $string = $twig->loadTemplate($string);
+
+        if ( is_array($data) )
+        {
+            $data = array_merge($data, $this->ci->load->get_vars());
+        }
+
+        if ( $return === true )
+        {
+            return $string->render($data);
+        }
+        else
+        {
+            return $string->display($data);
+        }
+
+    }
 
 }
