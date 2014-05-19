7b9dac52c2ef052adb96795a2f9f59db3718129e
Dwayne Charrington <dwayne@josephmark.com.au>
2014-05-19 16:12:09 +1000

    A whole plethora of changes. Support for Modular Extensions HMVC. Tested on Codeigniter 3.0. Multiple view path support, variable setting and getting.

--- config/twig.php
+++ config/twig.php
@@ -1,10 +1,34 @@
 <?php
+/**
+ * CI Twig
+ *
+ * Twig templating for Codeigniter with support for
+ * Modular Extensions HMVC
+ *
+ * @package   CI Twig
+ * @author      Dwayne Charrington
+ * @copyright  2014 Dwayne Charrington and Github contributors
+ * @link           http://ilikekillnerds.com
+ * @license     Licenced under MIT
+ * @version     1.1
+ */
 
 // Twig template extension (default)
 $config['twig.extension'] = ".twig";
 
-// Default template location
-$config['twig.location'] = APPPATH . "views/";
+// Default template locations
+$config['twig.locations'] = array(
+    APPPATH . "views/" => "../views/",
+    FCPATH . "views/"   => "../../views/"
+);
+
+$config['twig.functions'] = array(
+    // Register functions for use in Twig templates here
+);
+
+$config['twig.filters'] = array(
+    // Register filters for use in Twig templates here
+);
 
 // Twig template caching location
 $config['twig.cache_location'] = APPPATH . "cache/twig/";
--- libraries/Twig.php
+++ libraries/Twig.php
@@ -1,60 +1,170 @@
 <?php
+/**
+ * CI Twig
+ *
+ * Twig templating for Codeigniter with support for
+ * Modular Extensions HMVC
+ *
+ * @package   CI Twig
+ * @author      Dwayne Charrington
+ * @copyright  2014 Dwayne Charrington and Github contributors
+ * @link           http://ilikekillnerds.com
+ * @license     Licenced under MIT
+ * @version     1.1
+ */
 
 class Twig {
 
     protected $CI;
 
     protected $_twig;
+    protected $_twig_loader;
 
-    protected $_template_dir;
+    protected $_template_directories = array();
+    protected $_local_vars                = array();
+    protected $_global_vars              = array();
+
     protected $_cache_dir;
     protected $_debug;
 
     public function __construct()
     {
+        if (!DEFINED("EXT")) define("EXT", ".php");
+
         ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . APPPATH . 'third_party/Twig/lib/Twig');
         require_once (string) "Autoloader" . EXT;
 
+        // Fire off the Twig register bootstrap function...
+        Twig_Autoloader::register();
+
         // Get CI instance
-        $this->CI =& get_instance();
+        $this->CI = get_instance();
 
         // Load the Twig config file
         $this->CI->config->load('twig');
 
+        // Add in default Twig locations
+        $this->add_template_location(config_item('twig.locations'));
+
+        // Get locations
+        $this->_set_template_locations();
+
         // Required Twig paths and variables, modify in config/twig.php
-        $this->_template_dir = config_item('twig.location');
-        $this->_cache_dir    = config_item('twig.cache_location');
+        $this->_cache_dir  = config_item('twig.cache_location');
         $this->_debug        = config_item('twig.debug');
 
-        Twig_Autoloader::register();
+        $this->_twig_loader = new Twig_Loader_Filesystem($this->_template_directories);
 
-        $loader = new Twig_Loader_Filesystem($this->_template_dir);
-
-        $this->_twig = new Twig_Environment($loader, array(
+        $this->_twig = new Twig_Environment($this->_twig_loader, array(
                 'cache' => $this->_cache_dir,
                 'debug' => $this->_debug,
         ));
 
+        if ( !empty(config_item("twig.functions")) )
+        {
+            foreach ( config_item("twig.functions") AS $function )
+            {
+                $this->register_function($function);
+            }
+        }
+
+        if ( !empty(config_item("twig.filters")) )
+        {
+            foreach ( config_item("twig.filters") AS $filter )
+            {
+                $this->register_filter($filter);
+            }
+        }
+
     }
 
     /**
-     * Override the default template location
+     * Set
      *
-     * @param mixed $location
-     * @returns void
+     * Allows setting variables (both local and global) for Twig
+     * templates to access and use without requiring assignment
+     * via the parse method
+     *
+     * @param mixed $key
+     * @param mixed $value
+     * @param bool   $global
+     * @returns object
+     *
      */
-    public function set_location($location)
+    public function set($key, $value = "", $global = FALSE)
     {
-        $this->_template_dir = $location;
+        // Do we have an array of values to set?
+        if ( is_array($key) )
+        {
+            foreach ($key AS $k => $v)
+            {
+                if (!$global)
+                {
+                    $this->_local_vars[$k] = $v;
+                }
+                else
+                {
+                    $this->_twig->addGlobal($k, $v);
+                    $this->_global_vars[$k] = $v;
+                }
+            }
+        }
+        else
+        {
+            if ( !$global )
+            {
+                $this->_local_vars[$key] = $value;
+            }
+            else
+            {
+                $this->_twig->addGlobal($key, $value);
+                $this->_global_vars[$key] = $value;
+            }
+        }
+
+        // Return class for chaining
+        return $this;
     }
 
     /**
-    * Load the template and return the data
-    *
-    * @param mixed $template
-    * @param mixed $data
-    * @returns string
-    */
+     * Unset
+     *
+     * Unset a local or global variable
+     *
+     * @param mixed $key
+     * @param bool $global
+     * @returns object
+     *
+     */
+    public function unset_var($key, $global = FALSE)
+    {
+        if ( !$global )
+        {
+            if ( array_key_exists($key, $this->_local_vars) )
+            {
+                unset($this->_local_vars[$key]);
+            }
+        }
+        else
+        {
+            if ( array_key_exists($key, $this->_global_vars) )
+            {
+                unset($this->_global_vars[$key]);
+            }
+        }
+
+        // Return class for chaining
+        return $this;
+    }
+
+    /**
+     * Load the template and return the data
+     *
+     * @param mixed $template
+     * @param mixed $data
+     * @returns string
+     *
+     */
     public function parse($template, $data = array(), $return = FALSE)
     {
         if (stripos($template, '.') === FALSE)
@@ -82,6 +192,7 @@ class Twig {
      * @param mixed $data
      * @param mixed $return
      * @returns void
+     *
      */
     public function parse_string($string, $data = array(), $return = false)
     {
@@ -95,7 +206,149 @@ class Twig {
         {
             return $string->display($data);
         }
+    }
 
+    /**
+     * Register Function
+     *
+     * Alllows you to register functions for use within
+     * the Twig environment.
+     *
+     * @param mixed $name
+     * @returns object
+     *
+     */
+    public function register_function($name)
+    {
+        $this->_twig->addFunction($name, new Twig_Function_Function($name));
+
+        // Return class for chaining
+        return $this;
+    }
+
+    /**
+     * Register Filter
+     *
+     * Alllows you to register filters for use within
+     * the Twig environment.
+     *
+     * @param mixed $name
+     * @returns object
+     *
+     */
+    public function register_filter($name)
+    {
+        $this->_twig->addFilter($name, new Twig_Filter_Function($name));
+
+        // Return class for chaining
+        return $this;
+    }
+
+    /**
+     * __get
+     *
+     * A PHP magic function that catches all requests for
+     * something that might not exist.
+     *
+     * @param mixed $key
+     * @return array or boolean on false
+     *
+     */
+    public function __get($key)
+    {
+        if ( array_key_exists($key, $this->_global_vars) )
+        {
+            return $this->_global_vars[$key];
+        }
+        elseif ( array_key_exists($key, $this->_local_vars) )
+        {
+            return $this->_local_vars[$key];
+        }
+
+        // Not found
+        return FALSE;
+    }
+
+    /**
+     * __set
+     *
+     * A PHP magic function that catches all requests trying
+     * to set a variable that doesn't exist or isn't public
+     *
+     * @param mixed $key
+     * @return array or boolean on false
+     *
+     */
+    public function __set($key, $value)
+    {
+        if ( ! array_key_exists($key, $this->_local_vars) )
+        {
+            $this->_local_vars[$key] = $value;
+        }
+    }
+
+    /**
+     * Add Template Location
+     *
+     * Adds a new template location to the template
+     * locations array. Allows adding of template folders
+     * before view loading for example.
+     *
+     * @param mixed $location
+     * @returns object
+     *
+     */
+    public function add_template_location($location_var)
+    {
+        if ( is_array($location_var) )
+        {
+            foreach ($location_var AS $location => $offset)
+            {
+                if ( is_dir($location) )
+                {
+                    $this->_template_directories[] = $location;
+                }
+            }
+        }
+        else
+        {
+            if ( is_dir($location_var) )
+            {
+                $this->_template_directories[] = $location_var;
+            }
+        }
+    }
+
+    /**
+     * Set Template Locations
+     *
+     * Iterate over all modules and add their paths in
+     *
+     */
+    private function _set_template_locations()
+    {
+        if ( method_exists($this->CI->router, 'fetch_module') )
+        {
+            $this->_module = $this->CI->router->fetch_module();
+
+            if ( $this->_module )
+            {
+                $module_locations = Modules::$locations;
+
+                foreach ($module_locations AS $location => $offset)
+                {
+                    if ( is_dir($location . $this->_module . '/views') )
+                    {
+                        $this->_template_directories[] = $location . $this->_module . '/views';
+                    }
+                }
+            }
+        }
+
+        if ( $this->_twig_loader )
+        {
+            $this->_twig_loader->setPaths($this->_template_directories);
+        }
     }
 
 }
